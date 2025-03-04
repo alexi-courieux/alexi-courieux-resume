a12a1ae7d9189e7b0cf5232cb3c0255744495ded
alexi-courieux <contact@alexicourieux.com>
2025-03-04 08:13:59 -0500

    fix: add Lato font styles for improved typography

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,11 @@ import './i18n.tsx'
 import { I18nContextProvider } from './contextProviders/I18nContextProvider.tsx'
 import { ThemeProvider } from './contextProviders/ThemeContextProvider.tsx'
 
+import '@fontsource/lato/300.css'
+import '@fontsource/lato/400.css'
+import '@fontsource/lato/500.css'
+import '@fontsource/lato/700.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <I18nContextProvider>
